8a43c1def4237da8b53f20c333cd82a4971c7855
Max Wang <maxwang051@gmail.com>
2018-04-15 14:23:53 -0500

    made some progress on the android app

--- android/app/src/main/java/sd/streaming/MainActivity.java
+++ android/app/src/main/java/sd/streaming/MainActivity.java
@@ -11,12 +11,15 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -26,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
     int count;
     ImageView imageView;
 
-    Mat img, warpSrc, warpDst, M, Minv, warped, gray, hls, lab;
+    List<Mat> hls_list;
+    Mat img, warpSrc, warpDst, M, Minv, warped, gray, hls, lab, hls_l, lab_b;
+    Mat l_thresh_high, b_thresh_high, sobel_x;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -35,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
                 case LoaderCallbackInterface.SUCCESS:
                 {
                     Log.i(TAG, "OpenCV loaded successfully");
-                    img = new Mat();
+                    img = new Mat(160,320,CvType.CV_8U);
+
                     warpSrc = new Mat();
                     warpSrc.create(4, 2, CvType.CV_32F);
 
@@ -45,11 +51,19 @@ public class MainActivity extends AppCompatActivity {
                     M = new Mat();
                     M.create(3,3, CvType.CV_32F);
                     Minv = new Mat();
-                    warped = new Mat();
+                    warped = new Mat(0, 0, CvType.CV_32F);
 
-                    gray = new Mat();
-                    hls = new Mat();
-                    lab = new Mat();
+                    gray = new Mat(0, 0, CvType.CV_32F);
+                    hls = new Mat(0, 0, CvType.CV_32F);
+                    lab = new Mat(0, 0, CvType.CV_32F);
+
+                    hls_l = new Mat(0, 0, CvType.CV_32F);
+                    lab_b = new Mat(0, 0, CvType.CV_32F);
+
+                    l_thresh_high = new Mat(0, 0, CvType.CV_32F);
+                    b_thresh_high = new Mat(0, 0, CvType.CV_32F);
+
+                    sobel_x = new Mat(0, 0, CvType.CV_8S);
                 } break;
                 default:
                 {
@@ -102,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
 
         Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.image0);
-        //Mat source = new Mat();
+
         try {
             img = Utils.loadResource(MainActivity.this, id);
             Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA);
@@ -110,15 +124,16 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG,Log.getStackTraceString(e));
         }
 
-        warped.create(img.cols(), img.rows(), CvType.CV_32F);
-        gray.create(img.cols(), img.rows(), CvType.CV_32F);
-        hls.create(img.cols(), img.rows(), CvType.CV_32F);
-        lab.create(img.cols(), img.rows(), CvType.CV_32F);
+        // Create matrices for these for some reason
+        //warped.create(img.rows(), img.cols(), CvType.CV_32F);
+        //hls_l.create(img.rows(), img.cols(), CvType.CV_32F);
+        //lab_b.create(img.rows(), img.cols(), CvType.CV_32F);
+        //l_thresh_high.create(img.rows(), img.cols(), CvType.CV_32F);
 
         processImage(img);
 
-        Bitmap bm = Bitmap.createBitmap(gray.cols(), gray.rows(),Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(gray, bm);
+        Bitmap bm = Bitmap.createBitmap(sobel_x.cols(), sobel_x.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(sobel_x, bm);
         imageView.setImageBitmap(bm);
 
         count = (count + 1) % 64;
@@ -128,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
         int height = img.height();
         int width = img.width();
 
+        // Warp perspective to top down
         int offset = 40;
 
         warpSrc.put(0, 0, new double[]{110,40});
@@ -145,10 +161,32 @@ public class MainActivity extends AppCompatActivity {
 
         Imgproc.warpPerspective(img, warped, M, new Size(width, height), Imgproc.INTER_LINEAR);
 
+        // Perform color transformations
         Imgproc.cvtColor(warped, gray, Imgproc.COLOR_RGB2GRAY);
         Imgproc.cvtColor(warped, hls, Imgproc.COLOR_RGB2HLS);
         Imgproc.cvtColor(warped, lab, Imgproc.COLOR_RGB2Lab);
 
+        // Get L and B channels from HLS and LAB
+        Core.extractChannel(hls, hls_l, 1);
+        Core.extractChannel(lab, lab_b, 2);
+
+        // Normalize lab_b if there is yellow in the image
+        Core.MinMaxLocResult b_minMax = Core.minMaxLoc(b_thresh_high);
+        if (b_minMax.maxVal > 175) {
+            Core.multiply(lab_b, new Scalar(255.0 / b_minMax.maxVal), lab_b);
+        }
+
+        // Threshold L and B channels
+        Imgproc.threshold(hls_l, l_thresh_high, 180, 255, 0);
+        Imgproc.threshold(lab_b, b_thresh_high, 180, 255, 0);
+
+        // Get Sobel in x direction
+        Imgproc.Sobel(gray, sobel_x, 5, 1, 0);
+        // Absolute value of Sobel
+        Core.absdiff(sobel_x, new Scalar(0), sobel_x);
+        // Normalize Sobel
+        Core.MinMaxLocResult sobel_minMax = Core.minMaxLoc(sobel_x);
+        Core.multiply(sobel_x, new Scalar(255.0 / sobel_minMax.maxVal), sobel_x);
     }
 
 }
