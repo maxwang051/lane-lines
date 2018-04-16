5fb27881418fb65ce1183a70172a7a610117a4ef
Eric Dailey <eric.dailey@utexas.edu>
2018-04-16 17:49:30 -0500

    steering angles

---
+++ log-data.py
@@ -0,0 +1,16 @@
+import h5py
+import scipy.misc
+import csv
+
+with h5py.File('../research/dataset/log/2016-01-30--11-24-51.h5') as hdf:
+	print('Keys: %s' % list(hdf.keys()))
+	key = 'steering_angle'
+	data = list(hdf[key])
+	print(len(data))
+	with open('steering_angles.csv', 'w') as file:
+		datawriter = csv.writer(file, lineterminator='\n')
+		freq = 0
+		for val in data:
+			if(freq%5==0):
+				datawriter.writerow([val])
+			freq+=1
